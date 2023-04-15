6fd189075b86bf38b114104c3cb82ce53b39e666
Shubhangini2 <shubhisingh1970@gmail.com>
2023-04-15 11:26:39 +0530

    task done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
-  
+    //Create a class A
+    public static class A{
+        //Create a method name meth having return type String
+       public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    //Create a class name B which extends class B
+    public static class B extends A{
+        @Override
+       public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+    //create object of class B and call method meth of class A with it
+    public static void main(String args[]){
+        B b1= new B();
+        b1.meth();
+    }
 }
